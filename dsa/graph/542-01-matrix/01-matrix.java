445eccbce16a5a54d22287d546441361cb423f58
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-04-05 00:25:10 +0530

    Time: 13 ms (84.60%) | Memory: 47.6 MB (34.16%) - LeetSync

--- dsa/graph/542-01-matrix/01-matrix.java
+++ dsa/graph/542-01-matrix/01-matrix.java
@@ -1,41 +1,32 @@
-import java.util.*;
 
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int rows = mat.length;
-        int cols = mat[0].length;
-        int[][] dist = new int[rows][cols];
+        int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
         Queue<int[]> queue = new LinkedList<>();
-        
-        // Step 1: Initialize distances and enqueue all zeroes
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (mat[i][j] == 0) {
-                    dist[i][j] = 0;
-                    queue.add(new int[]{i, j});
-                } else {
-                    dist[i][j] = Integer.MAX_VALUE; // Mark as unvisited
+        for(int i = 0; i< mat.length; i++){
+            for(int j = 0; j < mat[0].length; j++){
+                if(mat[i][j] == 0){
+                    queue.add(new int[]{i,j});
+                }else{
+                    mat[i][j] = Integer.MAX_VALUE;
                 }
             }
         }
-        
-        // Step 2: BFS from all zeroes
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            int r = cell[0], c = cell[1];
-            for (int[] dir : directions) {
-                int nr = r + dir[0];
-                int nc = c + dir[1];
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
-                    if (dist[nr][nc] > dist[r][c] + 1) {
-                        dist[nr][nc] = dist[r][c] + 1;
-                        queue.add(new int[]{nr, nc});
+        while(!queue.isEmpty()){
+            int[] temp = queue.poll();
+            int r = temp[0];
+            int c = temp[1];
+            for(int[] s : dir){
+                int nr = r + s[0];
+                int nc = c + s[1];
+                if(nr >= 0 && nr < mat.length && nc >= 0 && nc < mat[0].length){
+                    if(mat[nr][nc] > mat[r][c]+1){
+                        mat[nr][nc] = mat[r][c]+1;
+                        queue.add(new int[]{nr,nc});
                     }
                 }
             }
         }
-        
-        return dist;
+        return mat;
     }
 }
