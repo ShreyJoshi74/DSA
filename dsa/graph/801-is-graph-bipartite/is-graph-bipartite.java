f49e85d54ee05cf7761eb54cdcffc92505fc1f80
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-04-05 16:22:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (95.84%) - LeetSync

--- dsa/graph/801-is-graph-bipartite/is-graph-bipartite.java
+++ dsa/graph/801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,41 +1,31 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         char[] color = new char[graph.length];
-        for(int i = 0 ; i < graph.length; i++){
-            color[i] = 'N';
+        for (int i = 0; i < graph.length; i++) {
+            color[i] = 'N';  // N = Not colored
         }
-        for(int i = 0; i < graph.length; i++){
-            if(color[i] == 'N'){
-                if(!dfs(i,graph,color,0)){
+
+        for (int i = 0; i < graph.length; i++) {
+            if (color[i] == 'N') {
+                if (!dfs(i, graph, color, 'G')) {  // Start with color G
                     return false;
                 }
             }
         }
         return true;
     }
-    public boolean dfs(int node,int[][] graph, char[] color,int order){
-        if(color[node] == 'N'){
-            if(order % 2 == 0){
-                color[node] = 'G';
-                for(int s : graph[node]){
-                    if(color[s]=='G'){
-                        return false;
-                    }
-                }
-            }else{
-                color[node] = 'Y';
-                for(int s : graph[node]){
-                    if(color[s]=='Y'){
-                        return false;
-                    }
-                }
-            }
+
+    public boolean dfs(int node, int[][] graph, char[] color, char currentColor) {
+        if (color[node] != 'N') {
+            return color[node] == currentColor;
         }
-        for(int s : graph[node]){
-            if(color[s] == 'N'){
-                if(!dfs(s,graph,color,order+1)){
-                    return false;
-                }
+
+        color[node] = currentColor;
+        char oppositeColor = currentColor == 'G' ? 'Y' : 'G';
+
+        for (int neighbor : graph[node]) {
+            if (!dfs(neighbor, graph, color, oppositeColor)) {
+                return false;
             }
         }
         return true;
