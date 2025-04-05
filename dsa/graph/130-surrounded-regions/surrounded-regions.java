9f1aa74c683d2143002bf9aa8b46f60957278e0d
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-04-05 09:36:15 +0530

    Time: 2 ms (85.49%) | Memory: 44.8 MB (99.77%) - LeetSync

--- dsa/graph/130-surrounded-regions/surrounded-regions.java
+++ dsa/graph/130-surrounded-regions/surrounded-regions.java
@@ -1,25 +1,24 @@
 class Solution {
     public void solve(char[][] board) {
-        boolean[][] visited = new boolean[board.length][board[0].length];
         boolean[][] cannotBeVisited = new boolean[board.length][board[0].length];
         for(int i = 0 ; i < board.length; i++){
             if(board[i][0] == 'O'){
-                dfs(board,cannotBeVisited,i,0,visited);
+                dfs(board,cannotBeVisited,i,0);
             }
         }
         for(int i = 0 ; i < board.length; i++){
             if(board[i][board[0].length - 1] == 'O'){
-                dfs(board,cannotBeVisited,i,board[0].length-1,visited);
+                dfs(board,cannotBeVisited,i,board[0].length-1);
             }
         }
         for(int i = 0 ; i < board[0].length; i++){
             if(board[0][i] == 'O'){
-                dfs(board,cannotBeVisited,0,i,visited);
+                dfs(board,cannotBeVisited,0,i);
             }
         }
         for(int i = 0 ; i < board[0].length; i++){
             if(board[board.length - 1][i] == 'O'){
-                dfs(board,cannotBeVisited,board.length-1,i,visited);
+                dfs(board,cannotBeVisited,board.length-1,i);
             }
         }
         for(int i = 1 ; i < board.length-1 ; i++){
@@ -30,16 +29,16 @@ class Solution {
             }
         }
     }
-    public void dfs(char[][] board,boolean[][] cannotBeVisited, int r,int c,boolean[][] visited){
+    public void dfs(char[][] board,boolean[][] cannotBeVisited, int r,int c){
        int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
        cannotBeVisited[r][c] = true;
-       visited[r][c] = true;
+     
        for(int[] s : dir ){
         int nr = r + s[0];
         int nc = c + s[1];
         if(nr >= 0 && nr < board.length && nc >=0 && nc < board[0].length){
-            if(board[nr][nc] == 'O' && !visited[nr][nc]){
-                dfs(board,cannotBeVisited,nr,nc,visited);
+            if(board[nr][nc] == 'O' && !cannotBeVisited[nr][nc]){
+                dfs(board,cannotBeVisited,nr,nc);
             }
         }
        }
