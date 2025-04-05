d35b226cc3f36bcc68ca42bba478f8f24eb9fe52
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-04-05 11:23:00 +0530

    Time: 12 ms (14.95%) | Memory: 65.4 MB (5.27%) - LeetSync

--- dsa/graph/1073-number-of-enclaves/number-of-enclaves.java
+++ dsa/graph/1073-number-of-enclaves/number-of-enclaves.java
@@ -1,8 +1,10 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-         int rows = grid.length;
+        int rows = grid.length;
         int cols = grid[0].length;
         boolean[][] cannotBeVisited = new boolean[rows][cols];
+
+        // Traverse border cells
         for (int i = 0; i < rows; i++) {
             if (grid[i][0] == 1) {
                 dfs(grid, i, 0, cannotBeVisited);
@@ -19,28 +21,33 @@ class Solution {
                 dfs(grid, rows - 1, j, cannotBeVisited);
             }
         }
+
+        // Count remaining 1s that are not marked
         int count = 0;
-        for(int i = 1 ; i < grid.length-1 ; i++){
-            for (int j = 1 ; j < grid[0].length-1 ; j++){
-                if(grid[i][j]==1 && !cannotBeVisited[i][j]){
+        for (int i = 1; i < rows - 1; i++) {
+            for (int j = 1; j < cols - 1; j++) {
+                if (grid[i][j] == 1 && !cannotBeVisited[i][j]) {
                     count++;
                 }
             }
         }
+
         return count;
-        
     }
-    public void dfs(int[][] grid, int r, int c, boolean[][] cannotBeVisited){
-        cannotBeVisited[r][c]= true;
-        int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
-        for(int[] s : dir){
-            int nr = r + s[0];
-            int nc = c + s[1];
-            if(nr >= 0 && nr < grid.length && nc >=0 && nc < grid[0].length){
-                if(grid[nr][nc] == 1 && !cannotBeVisited[nr][nc]){
-                    grid[nr][nc] = 0;
-                    dfs(grid,nr,nc,cannotBeVisited);
-                }
+
+    public void dfs(int[][] grid, int r, int c, boolean[][] cannotBeVisited) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        cannotBeVisited[r][c] = true;
+
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for (int[] dir : directions) {
+            int nr = r + dir[0];
+            int nc = c + dir[1];
+            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols &&
+                grid[nr][nc] == 1 && !cannotBeVisited[nr][nc]) {
+                dfs(grid, nr, nc, cannotBeVisited);
             }
         }
     }
