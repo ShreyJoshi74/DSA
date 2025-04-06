5df1907324cfde162dcd6f39d891a543ba714af5
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-04-06 11:59:13 +0530

    Create CyclicDetectionDGC(BFS).java
    https://www.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
    
    Itna ydd rakho tum normal wali visited approach se directed graph me bfs nhi kar sakte?
    kyu?
    kyuki let's say Agr tum aaye node x pe 
    if (undirected) tum sab connected pe jaa sakte ho aur use visited mark kr diya
    if (directed) tum sab adj nhi ja sakte ho Aur upar se usse visited mark kr bethoge which is problematic

---
+++ dsa/graph/TopologicalSort/CyclicDetectionDGC(BFS).java
@@ -0,0 +1,24 @@
+ ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+             int[] inDegree = new int[V];
+             Queue<Integer> q = new LinkedList<>();
+             for(int i = 0 ; i < V ; i++){
+                 inDegree[i] = 0;
+                 adj.add(new ArrayList<>());
+             }
+             for(int[] ed : edges){
+                 inDegree[ed[1]]++;
+                 adj.get(ed[0]).add(ed[1]);
+             }
+             for(int i = 0; i < V; i++) {
+                if(inDegree[i] == 0) q.add(i);
+             }
+             ArrayList<Integer> output = new ArrayList<>();
+             while(!q.isEmpty()){
+                 int temp = q.poll();
+                 output.add(temp);
+                 for(int i : adj.get(temp)){
+                     inDegree[i]--;
+                     if(inDegree[i] == 0) q.add(i);
+                 }
+             }
+             return output;
