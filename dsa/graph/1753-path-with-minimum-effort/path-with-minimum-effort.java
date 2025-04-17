9ef623b502fe03423e092e4b4997a26fca302912
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-04-17 15:57:17 +0530

    Time: 41 ms (70.83%) | Memory: 45.1 MB (79.32%) - LeetSync

--- dsa/graph/1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ dsa/graph/1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -3,37 +3,35 @@ class Solution {
         int row = heights.length;
         int col = heights[0].length;
         int[][] dist = new int[row][col];
-        for (int[] d : dist) {
-            Arrays.fill(d, Integer.MAX_VALUE);
+        for (int[] rows : dist) {
+            Arrays.fill(rows, Integer.MAX_VALUE);
         }
 
-        int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]); // [effort, i, j]
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0],b[0]));
+        pq.add(new int[]{0,0,0});
         dist[0][0] = 0;
-        pq.add(new int[]{0, 0, 0});
-
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int effort = curr[0];
-            int i = curr[1];
-            int j = curr[2];
-
-            if (i == row - 1 && j == col - 1) return effort;
-
-            for (int[] dir : directions) {
+        int[][] direction = {{0,1},{0,-1},{1,0},{-1,0}};
+        while(!pq.isEmpty()){
+            int[] temp = pq.poll();
+            int i = temp[1];
+            int j = temp[2];
+            int effort = temp[0];
+            if(i == row-1 && j == col -1) return effort;
+            for(int[] dir : direction){
                 int ni = i + dir[0];
                 int nj = j + dir[1];
-                if (ni >= 0 && ni < row && nj >= 0 && nj < col) {
-                    int diff = Math.abs(heights[ni][nj] - heights[i][j]);
-                    int maxEffort = Math.max(effort, diff);
-                    if (maxEffort < dist[ni][nj]) {
+                if(ni >= 0 && ni < row && nj >= 0 && nj < col){
+                    int maxEffort = heights[ni][nj] - heights[i][j];
+                    if(maxEffort < 0) maxEffort = maxEffort * -1;
+                    maxEffort = Integer.max(effort,maxEffort);
+                    if(maxEffort < dist[ni][nj]){
                         dist[ni][nj] = maxEffort;
-                        pq.add(new int[]{maxEffort, ni, nj});
+                        pq.add(new int[]{maxEffort,ni,nj});
                     }
+
                 }
             }
         }
-
-        return -1; // Should never hit here
+        return -1; 
     }
 }
