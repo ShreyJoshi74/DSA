57ddccfce8261bbf8fdf306915746c411fa38e93
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-04-24 19:34:05 +0530

    Time: 12 ms (56.54%) | Memory: 45 MB (51.64%) - LeetSync

--- slidingWindow/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ slidingWindow/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,25 +1,20 @@
 class Solution {
-    public int maxFreq(int[] hash){
-        int maxIndex = 0;
-        for(int i = 0; i < 26; i++){
-            if(hash[i] > hash[maxIndex]){
-                maxIndex = i;
-            }
-        }
-        return hash[maxIndex];
-    }
+    
 
     public int characterReplacement(String s, int k) {
         int j = 0;
         int i = 0;
-        int maxCount = 0;
+        int maxFreq = 0;
         int count = 0;
-        int changes;
+        int maxCount = 0;
+       
         int[] hash =new int[26];
         Arrays.fill(hash,0);
         for(i = 0; i < s.length(); i++){
             hash[s.charAt(i) - 'A']++;
-            while(i - j + 1 - maxFreq(hash) > k){
+            maxFreq = Math.max(hash[s.charAt(i)-'A'],maxFreq);
+
+            if(i - j + 1 - maxFreq > k){
                 hash[s.charAt(j) - 'A']--;
                 j++;
             }
