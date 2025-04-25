37d628e1076b90e08f5d14640b9b7adcfbeb3e8e
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-04-25 21:34:35 +0530

    Time: 13 ms (62.10%) | Memory: 45.1 MB (21.80%) - LeetSync

---
+++ slidingWindow/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -0,0 +1,31 @@
+// The Potli Approach
+// a , b , c ko imagine kro ek potli me bandh liya aur min index a,b,c ki nikalo
+// phir count update kro "min index + 1"
+class Solution {
+    public int min(int a,int b,int c){
+        if (a < b && a < c){
+            return a;
+        }else if(b < a && b < c){
+            return b;
+        }else{
+            return c;
+        }
+    }
+    public int numberOfSubstrings(String s) {
+        int a = -1,b = -1,c = -1;
+        int r  = 0;
+        int t;
+        int cnt = 0;
+        while(r < s.length()){
+            if(s.charAt(r) == 'a') a = r;
+            if(s.charAt(r) == 'b') b = r;
+            if(s.charAt(r) == 'c') c = r;
+            t = min(a,b,c);
+            if(a != -1 && b != -1 && c != -1){
+                cnt += t + 1;
+            }
+            r++;
+        }
+        return cnt;
+    }
+}
