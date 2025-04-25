ace7eac01605dc4dd6512080b232011ad94a20e8
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-04-25 20:38:21 +0530

    Time: 14 ms (40.40%) | Memory: 54.5 MB (90.41%) - LeetSync

---
+++ slidingWindow/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int subArrays(int[] nums,int k){
+        int r = 0, j = 0, cnt = 0, sum = 0;
+        while( r < nums.length){
+            if(k < 0) return 0;
+            if(nums[r] % 2 != 0) cnt++;
+            while(cnt > k && j < nums.length){
+                if(nums[j] % 2 != 0) cnt--;
+                j++;
+            }
+            sum = sum + r - j + 1;
+            r++;
+        }
+        return sum;
+    }
+    public int numberOfSubarrays(int[] nums, int k) {
+            return subArrays(nums,k) - subArrays(nums,k-1);
+    }
+}
