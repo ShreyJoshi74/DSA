49175d81da5bef5194a5b3aff7faa9fe6386cfe4
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-04-26 10:11:13 +0530

    Time: 48 ms (65.44%) | Memory: 48.3 MB (6.27%) - LeetSync

--- slidingWindow/1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ slidingWindow/1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -5,41 +5,27 @@
 // making it for < k result = subArrays(k) - subArrays(k-1)
 
 class Solution {
-    
-    public int subArrays(int[] nums,int k){
-        Map<Integer,Integer> map = new HashMap<>();
-        int r = 0 ,cnt = 0,j = 0;
-        while(r < nums.length){
-            if(k < 0) return 0;
-            if(map.containsKey(nums[r])){
-                map.put(nums[r],map.get(nums[r])+1);
-                
-            }else{
-                map.put(nums[r],1);
+    public int subArrays(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int r = 0, cnt = 0, j = 0;
+        while (r < nums.length) {
+            map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
 
-            }
-            while(map.keySet().size() > k){
-                int temp = map.get(nums[j]);
-                if(temp == 1){
+            while (map.size() > k) {
+                map.put(nums[j], map.get(nums[j]) - 1);
+                if (map.get(nums[j]) == 0) {
                     map.remove(nums[j]);
-                    
-                }else{
-                    temp--;
-                    map.put(nums[j],temp);
                 }
                 j++;
-
             }
-            cnt += r - j + 1;
-        
-            r++;
-            
 
-            
+            cnt += r - j + 1;
+            r++;
         }
         return cnt;
     }
+
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return subArrays(nums,k) - subArrays(nums,k-1);
+        return subArrays(nums, k) - subArrays(nums, k - 1);
     }
 }
