6344ab0fc8788987c050cc1cc25392d06f517837
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-06-13 14:09:16 +0530

    Time: 1 ms (99.45%) | Memory: 45.4 MB (32.99%) - LeetSync

--- greedy/45-jump-game-ii/jump-game-ii.java
+++ greedy/45-jump-game-ii/jump-game-ii.java
@@ -1,14 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
-        int k = nums.length;
-        int[] dis = new int[nums.length];
-        Arrays.fill(dis,Integer.MAX_VALUE);
-        dis[0] = 0;
-        for(int i = 0; i < nums.length; i++){
-            for(int j = 1; j <= nums[i]; j++){
-                if(j+ i < k && 1 + dis[i] < dis[j + i]) dis[j + i] = dis[i] + 1;
+        int jumps = 0, farthest = 0, end = 0;
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            
+            if (i == end) {
+                jumps++;
+                end = farthest;
             }
         }
-        return dis[nums.length-1] != Integer.MAX_VALUE ?  dis[nums.length-1] : -1;
+        
+        return jumps;
     }
 }
