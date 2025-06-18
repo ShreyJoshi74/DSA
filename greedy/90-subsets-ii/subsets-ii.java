ae7bf27a0fcc79cc3965c9639dc2aabfb25e6f98
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-06-18 22:10:40 +0530

    Time: 2 ms (31.37%) | Memory: 43.3 MB (95.75%) - LeetSync

--- greedy/90-subsets-ii/subsets-ii.java
+++ greedy/90-subsets-ii/subsets-ii.java
@@ -1,21 +1,24 @@
 class Solution {
-    public void combi(int[] nums,int i,List<Integer> temp,List<List<Integer>> ls){
-        if(i == nums.length){
-            if(!ls.contains(temp))ls.add(new ArrayList<>(temp));
+    public void combi(int[] nums, int i, List<Integer> temp, Set<List<Integer>> ls) {
+        if (i == nums.length) {
+            ls.add(new ArrayList<>(temp));
             return;
         }
-        combi(nums,i+1,temp,ls);
-        temp.add(nums[i]);
-        combi(nums,i+1,temp,ls);
-        temp.remove(temp.size()-1);// backtracking
-        return;
 
+        // Don't include nums[i]
+        combi(nums, i + 1, temp, ls);
+
+        // Include nums[i]
+        temp.add(nums[i]);
+        combi(nums, i + 1, temp, ls);
+        temp.remove(temp.size() - 1); // backtrack
     }
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> ls = new ArrayList<>();
+        Arrays.sort(nums); // Sort to group duplicates
+        Set<List<Integer>> set = new HashSet<>();
         List<Integer> temp = new ArrayList<>();
-        combi(nums,0,temp,ls);
-        return new ArrayList<>(ls);
+        combi(nums, 0, temp, set);
+        return new ArrayList<>(set); // Convert set back to list
     }
 }
