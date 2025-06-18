389cb4cba17c307f42bb1f9110eb98ee711c7bc3
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-06-18 17:43:42 +0530

    Time: 2 ms (47.08%) | Memory: 43.3 MB (59.25%) - LeetSync

--- greedy/22-generate-parentheses/generate-parentheses.java
+++ greedy/22-generate-parentheses/generate-parentheses.java
@@ -1,13 +1,13 @@
 class Solution {
-    public void generater(int n, List<String> ls,String p,int right,int left){
+    public void generater( List<String> ls,String p,int right,int left){
         if(right==0 && left == 0) ls.add(p);
-        if(right > 0) generater(n,ls,p + "(",right-1,left+1);
-        if(left > 0) generater(n,ls,p + ")",right,left-1);
+        if(right > 0) generater(ls,p + "(",right-1,left+1);
+        if(left > 0) generater(ls,p + ")",right,left-1);
 
     }
     public List<String> generateParenthesis(int n) {
         List<String> ls = new ArrayList<>();
-        generater(n,ls,"",n,0);
+        generater(ls,"",n,0);
         return ls;
     }
 }
