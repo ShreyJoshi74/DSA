171b14fd1d7e80b6a56eab66ed954df91cc003fd
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-06-19 13:42:33 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (88.29%) - LeetSync

--- greedy/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ greedy/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,22 +1,24 @@
 class Solution {
-    public void combi(String digits,int i,String[] dic,List<String> ls,String temp){
-        if(i == digits.length()){
-            ls.add(temp);
+    public void combi(String digits, int i, String[] dic, List<String> ls, StringBuilder temp) {
+        if (i == digits.length()) {
+            ls.add(temp.toString());
             return;
         }
-        int index =  digits.charAt(i) - '0';
-        for(int j = 0; j < dic[index].length(); j++){
-            temp = temp + dic[index].charAt(j);
-            combi(digits,i+1,dic,ls,temp);
-            temp = temp.substring(0,temp.length()-1);
+
+        int index = digits.charAt(i) - '0';
+        for (int j = 0; j < dic[index].length(); j++) {
+            temp.append(dic[index].charAt(j));  // add
+            combi(digits, i + 1, dic, ls, temp);
+            temp.deleteCharAt(temp.length() - 1);  // backtrack
         }
     }
+
     public List<String> letterCombinations(String digits) {
         List<String> ls = new ArrayList<>();
-        if(digits.length() == 0) return ls;
-        String[] dic = {" "," ","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        String temp = "";
-        combi(digits,0,dic,ls,temp);
+        if (digits.length() == 0) return ls;
+
+        String[] dic = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        combi(digits, 0, dic, ls, new StringBuilder());
         return ls;
     }
 }
