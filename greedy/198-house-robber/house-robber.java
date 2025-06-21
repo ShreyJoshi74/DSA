a248b4d266548218faa41b7f9e5de5193e748f12
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-06-21 16:51:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (8.49%) - LeetSync

--- greedy/198-house-robber/house-robber.java
+++ greedy/198-house-robber/house-robber.java
@@ -2,13 +2,13 @@ class Solution {
     public int func(int[] nums,int[] dp,int index){
         if(index >= nums.length) return 0;
         if(dp[index] != -1) return dp[index];
-        for(int i = index; i < nums.length; i++){
-            int w1 = func(nums,dp,i + 1);
-            int w2 = nums[i] + func(nums,dp,i + 2);
-            dp[i] = Math.max(w1,w2);
-            return dp[i];
-        }
-        return -1;
+        
+            int w1 = func(nums,dp,index + 1);
+            int w2 = nums[index] + func(nums,dp,index + 2);
+            dp[index] = Math.max(w1,w2);
+            return dp[index];
+        
+
     }
     public int rob(int[] nums) {
         int[] dp = new int[nums.length];
