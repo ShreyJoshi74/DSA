91952cf48f87d012b368bf75931d814ed21ac7ac
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-06-21 11:42:50 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (91.58%) - LeetSync

---
+++ greedy/70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int func(int[] ways,int count){
+        if(count == 0) return 1;
+        if(count < 0) return 0;
+        if(ways[count] != -1) return ways[count];
+        int w1 = func(ways,count-1);
+        if(count -1 > -1) ways[count-1] = w1;
+        int w2 = func(ways,count-2);
+        if(count -2 >-1 )ways[count-2] = w2;
+        return w1 + w2;
+       
+
+    }
+    public int climbStairs(int n) {
+        int[] ways = new int[n + 1];
+        Arrays.fill(ways,-1);
+
+       
+        return func(ways,n);
+    }
+}
