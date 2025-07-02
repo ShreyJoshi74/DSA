b6ba9d6c38155ac0c1810511e9f9ecc1dc16edcf
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-07-02 11:42:09 +0530

    Time: 6 ms (11.76%) | Memory: 47.4 MB (80.89%) - LeetSync

---
+++ DynamicProgramming/64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,42 @@
+class Solution {
+    // public int roller(int[][] grid,int[][] dp,int i,int j){
+    //     if(i == grid.length -1 && j == grid[0].length-1) return grid[grid.length-1][grid[0].length-1];
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     int path = Integer.MAX_VALUE;
+    //     if(i + 1 <= grid.length -1) path =Math.min(path,grid[i][j] + roller(grid,dp, i+1, j));
+    //     if(j + 1 <= grid[0].length-1)  path =Math.min(path,grid[i][j] + roller(grid,dp,i,j+1));
+    //     dp[i][j] = path;
+    //     return dp[i][j];
+    // }
+    // public int minPathSum(int[][] grid) {
+    //     int m = grid.length;
+    //     int n = grid[0].length;
+    //     int[][] dp = new int[m][n];
+    //     for(int i = 0; i < m; i++){
+    //         Arrays.fill(dp[i],-1);
+    //     }
+    //     return roller(grid,dp,0,0);
+    // }
+    public int minPathSum(int[][] grid){
+        int m = grid.length;
+         int n = grid[0].length;
+         int[][] dp = new int[m][n];
+         for(int i = 0; i < m; i++){
+            Arrays.fill(dp[i],0);
+        }
+        dp[0][0] = grid[0][0];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) continue;
+                int path = Integer.MAX_VALUE;
+                if(i > 0) path = Math.min(path,dp[i-1][j] + grid[i][j]);
+                if(j > 0) path = Math.min(path,dp[i][j-1] + grid[i][j]);
+                dp[i][j] = path;
+
+            }
+        }
+        return dp[m-1][n-1];
+
+
+    }
+}
