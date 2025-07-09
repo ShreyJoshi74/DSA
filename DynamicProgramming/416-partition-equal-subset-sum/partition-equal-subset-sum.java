21451202a9d687bef5bda6ec053f6e13734cc6da
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-07-09 16:54:43 +0530

    Time: 45 ms (60.71%) | Memory: 46.7 MB (58.51%) - LeetSync

--- DynamicProgramming/416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ DynamicProgramming/416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,30 +1,22 @@
 class Solution {
-    public boolean f(int[] nums,int right,int i,Boolean[][] dp){
-        if(right == 0) {
-            return dp[i][right] = true;
-        }
-        if(i == 0){
-            // taken
-            if(nums[0] == right) return true;
-            return false;
-        }
-        if(dp[i][right] != null) return dp[i][right];
-        boolean notTaken = f(nums,right,i-1,dp);
-        boolean taken = false;
-        if(nums[i] <= right){
-            taken = f(nums,right - nums[i],i-1,dp);
-        }
-        return dp[i][right] = notTaken || taken;
-        
-    }
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int s : nums) sum += s;
+        for(int i: nums) sum += i;
         if(sum % 2 != 0) return false;
-        int target = sum / 2; // humne target ko aadha kr diya yaha pe
-        Boolean[][] dp = new Boolean[nums.length][target+1];
-        
-        return f(nums,target,nums.length -1,dp);
-
+        int target = sum / 2;
+        boolean[][] dp = new boolean[nums.length][target + 1];
+        for(int i = 0; i < nums.length; i++) dp[i][0] = true;
+        if(nums[0] <= target) dp[0][nums[0]] = true;
+        for(int i = 1; i < nums.length; i++){
+            for(int goal= 1; goal <= target; goal++){
+                //not taken
+                boolean notTaken = dp[i-1][goal];
+                //Taken
+                boolean taken = false;
+                if(nums[i] <= goal) taken = dp[i-1][goal - nums[i]];
+                dp[i][goal] = taken || notTaken;
+            }
+        }
+        return dp[nums.length - 1][target];
     }
 }
