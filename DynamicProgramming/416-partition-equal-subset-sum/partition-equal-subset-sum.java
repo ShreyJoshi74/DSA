344609d5a11c801287232ddce81f5f77def59257
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-07-09 16:18:57 +0530

    Time: 93 ms (23.16%) | Memory: 57.9 MB (16.19%) - LeetSync

--- DynamicProgramming/416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ DynamicProgramming/416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,25 +1,30 @@
 class Solution {
-    public boolean f(int left,int right,int i,int[] nums,Boolean[][] dp){
-        if(left == right) return true;
-        if(dp[i][right] != null) return dp[i][right];
-        if(i == 0 ){
-            left += nums[0];
-            right -= nums[0];
-            if(left == right) return true;
+    public boolean f(int[] nums,int right,int i,Boolean[][] dp){
+        if(right == 0) {
+            return dp[i][right] = true;
+        }
+        if(i == 0){
+            // taken
+            if(nums[0] == right) return true;
             return false;
-
         }
-        boolean notTaken = f(left,right,i-1,nums,dp);
+        if(dp[i][right] != null) return dp[i][right];
+        boolean notTaken = f(nums,right,i-1,dp);
         boolean taken = false;
-        if(nums[i] <= right - nums[i]) {
-            taken = f(left + nums[i],right - nums[i],i-1,nums,dp);
+        if(nums[i] <= right){
+            taken = f(nums,right - nums[i],i-1,dp);
         }
-        return dp[i][right] = taken || notTaken;
+        dp[i][right] = notTaken || taken;
+        return notTaken || taken;
     }
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int i : nums) sum += i;
-        Boolean[][] dp = new Boolean[nums.length][sum+1];
-        return f(0,sum,nums.length-1,nums,dp);
+        for(int s : nums) sum += s;
+        if(sum % 2 != 0) return false;
+        int target = sum / 2; // humne target ko aadha kr diya yaha pe
+        Boolean[][] dp = new Boolean[nums.length][target+1];
+        
+        return f(nums,target,nums.length -1,dp);
+
     }
 }
