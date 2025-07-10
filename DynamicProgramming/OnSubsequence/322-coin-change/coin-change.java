dfd651ba9f308b1d576f0e141e0669707404fac9
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-07-10 22:06:00 +0530

    Time: 20 ms (38.75%) | Memory: 45.5 MB (17.99%) - LeetSync

--- DynamicProgramming/OnSubsequence/322-coin-change/coin-change.java
+++ DynamicProgramming/OnSubsequence/322-coin-change/coin-change.java
@@ -1,28 +1,25 @@
 class Solution {
-    public int f(int ind,int T,int[] coins,int[][] dp){
-        if(T == 0) return 0;
-
-        if(ind == 0){
-            if(T % coins[0] == 0) return T/coins[0];
-            else return (int)1e9;
-        }
-        if(dp[ind][T] != -1) return dp[ind][T];
-        int notTaken = f(ind - 1,T,coins,dp);
-        int taken = (int) 1e9;
-        if(coins[ind] <= T){
-            taken = 1 + f(ind,T - coins[ind],coins,dp);
-        }
-        return dp[ind][T] = Math.min(notTaken,taken);
-
-    }
     public int coinChange(int[] coins, int amount) {
-        
         int[][] dp = new int[coins.length][amount + 1];
-        
-        for(int i = 0; i < coins.length;i++){
-            Arrays.fill(dp[i],-1);
+        for(int i = 0; i < coins.length; i++){
+            Arrays.fill(dp[i],Integer.MAX_VALUE);
         }
-        int out = f(coins.length -1,amount,coins,dp);
-        return (out >= 1e9) ? -1 : out;
+        for(int t = 0; t <= amount ; t++){
+            if(t % coins[0] == 0) dp[0][t] = t / coins[0];
+            else dp[0][t] = (int) 1e9;
+        }
+        for(int i = 1; i < coins.length; i++){
+            for(int t = 0; t <=  amount; t++){
+                //taken
+                int taken = (int) 1e9;
+                if(t - coins[i] >= 0)
+                    taken = 1 + dp[i][t - coins[i]];
+                //not Taken
+                int notTaken = dp[i-1][t];
+                dp[i][t] = Math.min(taken,notTaken);
+            }
+        }
+        int output = dp[coins.length - 1][amount];
+        return (output != 1e9) ? output : -1;
     }
 }
