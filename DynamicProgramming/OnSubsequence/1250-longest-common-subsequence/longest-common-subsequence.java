a813fa455176abd3cd465c29276f5af44147c969
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-07-12 15:37:17 +0530

    Time: 40 ms (5.19%) | Memory: 51.1 MB (42.60%) - LeetSync

--- DynamicProgramming/OnSubsequence/1250-longest-common-subsequence/longest-common-subsequence.java
+++ DynamicProgramming/OnSubsequence/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,12 +1,7 @@
 class Solution {
     public int f(int i,int j,int[][] dp,String text1,String text2){
         if(i >= text1.length()  || j >= text2.length()) return 0;
-        if(i == text1.length() - 1 && j == text2.length() - 1){
-            if(text1.charAt(i) == text2.charAt(j)){
-                return 1;
-            }
-            return 0;
-        }
+        
         if(dp[i][j] != -1) return dp[i][j];
         //ek baar j 
         int jkeSath = 0;
