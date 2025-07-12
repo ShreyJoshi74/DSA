9a730ec1a12ebd3eba26ab520ba0e624ed017940
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-07-12 15:37:54 +0530

    Time: 38 ms (5.19%) | Memory: 51.1 MB (30.17%) - LeetSync

--- DynamicProgramming/OnSubsequence/1250-longest-common-subsequence/longest-common-subsequence.java
+++ DynamicProgramming/OnSubsequence/1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,12 +3,6 @@ class Solution {
         if(i >= text1.length()  || j >= text2.length()) return 0;
         
         if(dp[i][j] != -1) return dp[i][j];
-        //ek baar j 
-        int jkeSath = 0;
-        if(j  <= text2.length() - 1){
-            if(text1.charAt(i) == text2.charAt(j)) jkeSath = 1 + f(i+1,j+1,dp,text1,text2);
-            else jkeSath = f(i,j+1,dp,text1,text2);
-        }
         // ek baar i
         int ikeSath = 0;
         if(i  <= text1.length() - 1){
@@ -16,6 +10,13 @@ class Solution {
             else ikeSath = f(i+1,j,dp,text1,text2);
 
         }
+        //ek baar j 
+        int jkeSath = 0;
+        if(j  <= text2.length() - 1){
+            if(text1.charAt(i) == text2.charAt(j)) jkeSath = 1 + f(i+1,j+1,dp,text1,text2);
+            else jkeSath = f(i,j+1,dp,text1,text2);
+        }
+        
         return dp[i][j] = Math.max(jkeSath , ikeSath);
     }
     public int longestCommonSubsequence(String text1, String text2) {
