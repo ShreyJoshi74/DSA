db88e485f6a46e8fb667bc8499b6e81d35797d5b
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-07-13 01:04:50 +0530

    Time: 6 ms (88.75%) | Memory: 44.3 MB (55.74%) - LeetSync

--- DynamicProgramming/OnSubsequence/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ DynamicProgramming/OnSubsequence/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,23 +1,27 @@
 class Solution {
-    public int f(int i,int j,int[] nums,int[][] dp){
-        if(i == nums.length - 1){
-            if(j == -1 || nums[i] > nums[j]) return 1;
-            return 0;
+    public int giveposi(ArrayList<Integer> ls,int number){
+        int i = 0,j = ls.size();
+        while(i < j){
+           int  m = i + (j - i)/2;
+            if(ls.get(m) < number){
+                i = m + 1;
+            }else{
+                j = m;
+            }
         }
-        if(dp[i][j + 1] != -1) return dp[i][j+1];
-        // notTaken
-        int notTaken = f(i + 1,j,nums,dp);
-        int taken = 0;
-        if(j == -1 || nums[i] > nums[j]) taken = 1 + f(i + 1,i,nums,dp);
-        return dp[i][j + 1] = Math.max(notTaken,taken);
+        return i;
     }
+    
     public int lengthOfLIS(int[] nums) {
-        if(nums.length == 1) return 1;
-        int[][] dp = new int[nums.length][nums.length + 1];
-        for(int i = 0; i < nums.length;i++){
-            Arrays.fill(dp[i],-1);
+        ArrayList<Integer> ls = new ArrayList<>();
+        for(int k : nums){
+            int p = giveposi(ls,k);
+            if( p == ls.size()) ls.add(k);
+            else{
+                ls.set(p,k);
+            }
         }
-
-        return f(0,-1,nums,dp);
+        return ls.size();
+        
     }
 }
