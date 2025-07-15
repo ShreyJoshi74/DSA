f0a94b6f15889853b4f479fc7d356f1d76cdf23a
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-07-15 11:37:20 +0530

    Time: 55 ms (30.11%) | Memory: 44.1 MB (87.37%) - LeetSync

--- DynamicProgramming/OnSubsequence/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ DynamicProgramming/OnSubsequence/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,16 +1,44 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,1);
-        int max = 1;
-        for(int i = 0; i <nums.length; i++){
-            for(int j = 0; j < i ; j++){
-                if(nums[i] > nums[j]){
-                    dp[i] = Math.max(dp[i],dp[j] + 1);
-                    max = Math.max(max,dp[i]);
+        int n = nums.length;
+        int[] dp = new int[n];
+        int[] hash = new int[n];
+
+        // Initialization
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            hash[i] = i; // initially point to itself
+        }
+
+        int maxLen = 1;
+        int lastIndex = 0;
+
+        // Fill dp and hash arrays
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    hash[i] = j;
                 }
             }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                lastIndex = i;
+            }
         }
-        return max;
+
+        // Reconstruct the sequence
+        ArrayList<Integer> lis = new ArrayList<>();
+        lis.add(nums[lastIndex]);
+        while (hash[lastIndex] != lastIndex) {
+            lastIndex = hash[lastIndex];
+            lis.add(nums[lastIndex]);
+        }
+
+        // Print in correct order
+        Collections.reverse(lis);
+        System.out.println("LIS: " + lis);
+
+        return maxLen;
     }
 }
