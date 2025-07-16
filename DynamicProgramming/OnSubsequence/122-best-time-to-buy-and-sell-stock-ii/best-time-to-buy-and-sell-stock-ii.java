06ff9f186342fb00ed0432beb0e0eb2fc3322eae
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-07-16 13:44:24 +0530

    Time: 1 ms (78.39%) | Memory: 45.6 MB (94.03%) - LeetSync

--- DynamicProgramming/OnSubsequence/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ DynamicProgramming/OnSubsequence/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min = Integer.MAX_VALUE,profit = 0;
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < min) min = prices[i];
-            else if(i == prices.length - 1|| prices[i + 1] < prices[i] ){
-                profit += prices[i] - min;
-                min = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
             }
         }
         return profit;
