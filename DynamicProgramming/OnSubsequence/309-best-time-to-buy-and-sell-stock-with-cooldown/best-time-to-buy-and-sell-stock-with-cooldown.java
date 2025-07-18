1d7f7a8220ae7eceb7e46d25262b9ede261c73e2
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-07-18 13:45:26 +0530

    Time: 1 ms (82.62%) | Memory: 41.8 MB (69.70%) - LeetSync

--- DynamicProgramming/OnSubsequence/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ DynamicProgramming/OnSubsequence/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -3,32 +3,30 @@
 // 1 -> cooldown 0 -> can buy
 // 1 -> can buy 0 -> can sell
 class Solution {
-    public int f(int i,int action,int cooldown,int[] prices,int[][][] dp){
+    public int f(int i,int action,int[] prices,int[][]dp){
+        if(i >= prices.length) return 0;
         if(i == prices.length - 1){
-            if(cooldown == 1) return 0;
-            else{
+            
                 if(action == 0) return prices[i];
                 if(action == 1) return 0;
-            }
+            
         }
-        if(dp[i][action][cooldown] != -1) return dp[i][action][cooldown];
-        if(cooldown == 1){
-            return dp[i][action][cooldown] = f(i+1,action,0,prices,dp);
-        }else{
+        if(dp[i][action] != -1) return dp[i][action];
+        
             if(action == 1){
-                return dp[i][action][cooldown] = Math.max(-prices[i] + f(i+1,0,0,prices,dp),f(i+1,action,cooldown,prices,dp));
+                return dp[i][action] = Math.max(-prices[i] + f(i+1,0,prices,dp),f(i+1,action,prices,dp));
             }else{
-                return dp[i][action][cooldown] = Math.max(prices[i] + f(i+1,1,1,prices,dp),f(i+1,action,cooldown,prices,dp));
+                return dp[i][action] = Math.max(prices[i] + f(i+2,1,prices,dp),f(i+1,action,prices,dp));
             }
-        }
+        
     }
     public int maxProfit(int[] prices) {
-        int[][][] dp = new int[prices.length][2][2];
+        int[][] dp = new int[prices.length][2];
         for(int i = 0; i < prices.length; i++){
-            for(int j = 0; j < 2; j++){
-                Arrays.fill(dp[i][j],-1);
-            }
+            
+                Arrays.fill(dp[i],-1);
+            
         }
-        return f(0,1,0,prices,dp);
+        return f(0,1,prices,dp);
     }
 }
