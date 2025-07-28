e41e63cb45d6d1b0dfc8d2e16c4c3884920a65b7
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-07-28 23:24:10 +0530

    Time: 41 ms (80.55%) | Memory: 51.2 MB (52.91%) - LeetSync

---
+++ DynamicProgramming/OnSubsequence/943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -0,0 +1,30 @@
+class Solution {
+    // 3 , 1 , 2, 3, 2, 1
+    //3, 1 + 1,2 + 2, 
+    public int sumSubarrayMins(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int[] dp = new int[arr.length];
+        for(int curr= 0; curr < arr.length; curr++){
+            int popcnt = 0;
+            while(!stack.isEmpty() && arr[curr] <= arr[stack.peek()]){
+                popcnt++;
+                stack.pop();
+            }
+            int mul;
+            if(stack.isEmpty()) {
+                mul = curr + 1;
+                dp[curr] = (arr[curr] * mul)  % (int) (1e9 + 7);
+            }
+            else {
+                mul = curr - stack.peek() ;
+                dp[curr] = (arr[curr] * mul + dp[stack.peek()] )% (int) (1e9 + 7);
+            }
+
+            
+            stack.push(curr);
+        }
+        int output = 0;
+        for(int i = 0; i < arr.length; i++) output  = (output +  dp[i])  % (int) (1e9 + 7);
+        return output  % (int) (1e9 + 7);
+    }
+}
