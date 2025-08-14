7f54f82a065ff1785029822426a9d993ad83357e
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-08-14 10:16:03 +0530

    Time: 930 ms (21.32%) | Memory: 0B (100.00%) - LeetSync

--- binarySearch/1390-average-selling-price/average-selling-price.sql
+++ binarySearch/1390-average-selling-price/average-selling-price.sql
@@ -2,14 +2,8 @@
 WITH Table1 AS (SELECT A.product_id,A.price * B.units as Total,B.units AS units
 FROM UnitsSold AS B
 RIGHT JOIN Prices AS A
-    ON B.product_id = A.product_id AND B.purchase_date BETWEEN A.start_date AND A.end_date),
-Table2 AS (SELECT product_id,ROUND(SUM(Total)/SUM(units),2) AS average_price
+    ON B.product_id = A.product_id AND B.purchase_date BETWEEN A.start_date AND A.end_date)
+SELECT product_id,ifnull(ROUND(SUM(Total)/SUM(units),2),0) AS average_price
 FROM Table1
 GROUP BY product_id
-ORDER BY product_id ASC)
-SELECT product_id,
-    CASE
-        WHEN average_price IS NULL THEN 0
-        ELSE average_price
-    END AS average_price
-FROM Table2;
+ORDER BY product_id ASC;
