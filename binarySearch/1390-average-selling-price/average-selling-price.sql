b65b31bf460ee2ba0ace82dccef88c8e8b892434
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-08-14 10:14:21 +0530

    Time: 799 ms (51.35%) | Memory: 0B (100.00%) - LeetSync

---
+++ binarySearch/1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+WITH Table1 AS (SELECT A.product_id,A.price * B.units as Total,B.units AS units
+FROM UnitsSold AS B
+RIGHT JOIN Prices AS A
+    ON B.product_id = A.product_id AND B.purchase_date BETWEEN A.start_date AND A.end_date),
+Table2 AS (SELECT product_id,ROUND(SUM(Total)/SUM(units),2) AS average_price
+FROM Table1
+GROUP BY product_id
+ORDER BY product_id ASC)
+SELECT product_id,
+    CASE
+        WHEN average_price IS NULL THEN 0
+        ELSE average_price
+    END AS average_price
+FROM Table2;
