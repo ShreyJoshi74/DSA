83303d5467a72fa9ba487ac73d7812df1de061d0
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-08-18 17:59:40 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (66.03%) - LeetSync

---
+++ binarySearch/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNonDuplicate(int[] arr) {
+        int n = arr.length;
+        if(n == 1) return arr[0];
+        if(arr[0] != arr[1]) return arr[0];
+        if(arr[n-1] != arr[n-2]) return arr[n-1];
+        int l = 0,h = n - 1,mid = 0;
+        while(l <= h){
+            mid = (l + h)/2;
+            if(arr[mid-1] != arr[mid] && arr[mid+1] != arr[mid] ) return arr[mid];
+            if(mid % 2 == 0){
+                if(arr[mid] == arr[mid+1]) l = mid + 2;
+                else h = mid - 1;
+            }else{
+                if(arr[mid + 1] == arr[mid]) h = mid - 1;
+                else l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
