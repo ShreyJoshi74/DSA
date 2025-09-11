256a824f4e1b5c886e9d0cb31d27431de6ec2ddc
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-09-11 18:31:04 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (60.98%) - LeetSync

--- linkedList/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ linkedList/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,46 +2,28 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int cnt = -1;
-        int target2 = (n1 + n2) / 2;
-        int i = 0;
-        int j = 0;
-        double ele1 = -1;
-        double ele2 = -1;
-        while(i < n1 && j < n2){
-            if(nums1[i] <= nums2[j]){
-                ele1 = ele2;
-                ele2 = nums1[i++];
-                cnt++;
-            }else{
-                ele1 = ele2;
-                ele2 = nums2[j++];
-                cnt++;
+        if(n1 > n2) return findMedianSortedArrays(nums2,nums1);
+        int low = 0;
+        int high = n1;
+        int left = (n1 + n2 + 1)/2;
+        int n = n1 + n2;
+        while(low <= high){
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            r1 = (mid1 < n1) ? nums1[mid1] :Integer.MAX_VALUE;
+            r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
+            l1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            l2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[mid2 - 1];
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2 == 1) return Math.max(l1,l2);
+                return (double) (Math.max(l1,l2) + Math.min(r1,r2))/2;
             }
-            if(cnt == target2){
-                if((n1 + n2) % 2 == 0) return (ele1 + ele2)/2;
-                else return ele2;
-            }
-        }
-        while(i < n1){
-            ele1 = ele2;
-            ele2 = nums1[i++];
-            cnt++;
-            if(cnt == target2){
-                if((n1 + n2) % 2 == 0) return (ele1 + ele2)/2;
-                else return ele2;
-            }
-
-        }
-        while(j < n2){
-            ele1 = ele2;
-            ele2 = nums2[j++];
-            cnt++;
-            if(cnt == target2){
-                if((n1 + n2) % 2 == 0) return (ele1 + ele2)/2;
-                else return ele2;
-            }
-
+            else if(l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
         }
         return 0;
     }
