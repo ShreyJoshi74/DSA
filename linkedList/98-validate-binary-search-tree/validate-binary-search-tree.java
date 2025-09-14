abc743263055e9c16433a6bbfd16877a77646071
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-09-14 13:05:47 +0530

    Time: 1 ms (17.62%) | Memory: 43.5 MB (58.86%) - LeetSync

--- linkedList/98-validate-binary-search-tree/validate-binary-search-tree.java
+++ linkedList/98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,18 +14,33 @@
  * }
  */
 class Solution {
-    long prev = Long.MIN_VALUE;
-    public boolean isValid(TreeNode root){
-        if(root == null) return true;
-        boolean A = isValid(root.left);
-        boolean B = true;
-        if(prev >= root.val) B = false;
-        prev = root.val;
-        boolean C = isValid(root.right);
-        return A & B & C;
-    }
+    // long prev = Long.MIN_VALUE;
+    // public boolean isValid(TreeNode root){
+    //     if(root == null) return true;
+    //     boolean A = isValid(root.left);
+    //     boolean B = true;
+    //     if(prev >= root.val) B = false;
+    //     prev = root.val;
+    //     boolean C = isValid(root.right);
+    //     return A & B & C;
+    // }
     public boolean isValidBST(TreeNode root) {
-        if(root.left == null && root.right == null) return true;
-        return isValid(root);
+        // if(root.left == null && root.right == null) return true;
+        // return isValid(root);
+        Stack<TreeNode> st = new Stack<>();
+        long prev = Long.MIN_VALUE;
+        if(root == null) return true;
+        while(root != null || !st.isEmpty()){
+            while(root != null){
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+
+            if(root.val <= prev) return false;
+            prev = root.val;
+            root = root.right;
+        }
+        return true;
     }
 }
