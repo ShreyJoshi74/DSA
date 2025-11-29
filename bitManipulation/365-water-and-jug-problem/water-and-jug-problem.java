465c0f3635b882342ba38c27c4c20545395c1f91
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-11-29 21:59:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (35.53%) - LeetSync

--- bitManipulation/365-water-and-jug-problem/water-and-jug-problem.java
+++ bitManipulation/365-water-and-jug-problem/water-and-jug-problem.java
@@ -1,28 +1,18 @@
 class Solution {
-    public void findPossiblePath(List<Integer> ls , int x, int y){
-        if(x % y == 0) return;
-        ls.add(x % y);
-        findPossiblePath(ls,y,x%y);
-    }
-    public boolean canMeasureWater(int x, int y, int target) {
-        List<Integer> possiblePath = new ArrayList<>();
-        possiblePath.add(Math.max(x,y));
-        possiblePath.add(Math.min(x,y));
-        if(target > x + y) return false;
-        findPossiblePath(possiblePath,possiblePath.get(0),possiblePath.get(1));
-        Queue<Integer> search = new LinkedList<>();
-        search.add(0);
-        boolean[] visited = new boolean[target + 1];
-        while(!search.isEmpty()){
-            int currentSum = search.poll();
-            visited[currentSum] = true;
-            for(int nextMove : possiblePath){
-                int nextSum = currentSum + nextMove;
-                if(nextSum  == target) return true;
-                if(nextSum < target && !visited[nextSum]) search.add(nextSum);
-            }
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
         }
-        return false;
+        return a;
+    }
 
+    public boolean canMeasureWater(int x, int y, int target) {
+        if (target == 0) return true;
+        if (x + y < target) return false;
+        if (x == 0 || y == 0) return target == x || target == y;
+
+        return target % gcd(x, y) == 0;
     }
 }
