2ede8c44277a4c2a1318b6b206449ee2ad1b89fd
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-12-05 16:31:58 +0530

    Time: 12 ms (95.01%) | Memory: 74.2 MB (11.34%) - LeetSync

--- bitManipulation/2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.java
+++ bitManipulation/2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.java
@@ -1,29 +1,25 @@
 class Solution {
     public long maxRunTime(int n, int[] batteries) {
         long total = 0;
-        for (int b : batteries) {
-            total += b;
+        for(int ele : batteries) total += ele;
+        long high = total / n;
+        long low = 0;
+        long ans = -1;
+        while(low <= high){
+            long mid = (low + high) >> 1;
+            if(canRun(n,batteries,mid)) {
+                ans = mid;
+                low = mid+1;
+            }else high = mid - 1;
+
         }
-
-        long low = 0, high = total / n;
-
-        while (low < high) {
-            long mid = low + (high - low + 1) / 2;
-            if (canRun(mid, n, batteries)) {
-                low = mid;
-            } else {
-                high = mid - 1;
-            }
-        }
-
-        return low;
+        return ans;
     }
-
-    private boolean canRun(long time, int n, int[] batteries) {
-        long usable = 0;
-        for (int b : batteries) {
-            usable += Math.min((long) b, time);
+    public boolean canRun(int n, int[] batteries,long time){
+        long canrun = 0;
+        for(int battery : batteries){
+             canrun += Math.min(battery,time);
         }
-        return usable >= time * n;
+        return canrun >= n * time;
     }
 }
