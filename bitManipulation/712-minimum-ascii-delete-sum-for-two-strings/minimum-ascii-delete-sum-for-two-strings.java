7c49ea2d8efec9b3752d6a9ec204fe1ae63283e5
ShreyJoshi74 <shreyjoshi199@gmail.com>
2026-01-10 11:07:28 +0530

    Time: 20 ms (66.62%) | Memory: 46.6 MB (27.72%) - LeetSync

--- bitManipulation/712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ bitManipulation/712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -2,12 +2,12 @@ class Solution {
     public int func(int i, int j, String s1, String s2, int[][] dp){
         if(i == s1.length() && j == s2.length())   
             return 0;
-        if(i >= s1.length()){
+        if(i == s1.length()){
             int rem = 0;
             while(j < s2.length()) rem += (int) s2.charAt(j++);
             return rem;
         }
-        if(j >= s2.length()){
+        if(j == s2.length()){
             int rem = 0;
             while(i < s1.length()) rem += (int) s1.charAt(i++);
             return rem;
@@ -31,11 +31,41 @@ class Solution {
     }
     public int minimumDeleteSum(String s1, String s2) {
         int[][] dp = new int[s1.length() + 1][s2.length() + 1];
-        for(int i = 0; i < s1.length(); i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return func(0,0,s1,s2,dp);
+        // for(int i = 0; i < s1.length(); i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return func(0,0,s1,s2,dp);
+
         // trying bottom up approach
+
+        int lenS1 = s1.length();
+        int lenS2 = s2.length();
+        dp[lenS1][lenS2] = 0;
+
+        int rem = 0;
+        for(int j = lenS2 - 1; j >= 0; j--){
+            rem += (int) s2.charAt(j);
+            dp[lenS1][j] = rem;
+        }
+
+        rem = 0;
+        for(int i = lenS1 - 1; i >= 0; i--){
+            rem += (int) s1.charAt(i);
+            dp[i][lenS2] = rem;
+        }
+
+        for(int i = lenS1 - 1; i >= 0; i--){
+            for(int j = lenS2 - 1; j>=0; j--){
+                int take = Integer.MAX_VALUE;
+                if(s1.charAt(i) == s2.charAt(j)) take = dp[i+1][j+1];
+                dp[i][j] = Math.min(s1.charAt(i) + dp[i+1][j],
+                                Math.min(s2.charAt(j) + dp[i][j+1], take)
+                            );
+            }
+        }
+        return dp[0][0];
+
+
 
     }
 }
