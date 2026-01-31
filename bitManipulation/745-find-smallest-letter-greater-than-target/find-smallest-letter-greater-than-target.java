6e7acf87c078c75379c883f0cf96f6f2cc019fba
Shrey Joshi <shreyjoshi199@gmail.com>
2026-01-31 16:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (9.86%) - LeetSync

--- bitManipulation/745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ bitManipulation/745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -2,12 +2,12 @@ class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         
         
-        int ans = Integer.MAX_VALUE;
+        int ans = 'z' + 1;
         for(int i = 0; i < letters.length; i++){
             if(target < letters[i] && letters[i] < ans)
                 ans = letters[i] ;
         }
         
-        return ans == Integer.MAX_VALUE ? letters[0] : (char) ans;
+        return ans == 'z' + 1 ? letters[0] : (char) ans;
     }
 }
