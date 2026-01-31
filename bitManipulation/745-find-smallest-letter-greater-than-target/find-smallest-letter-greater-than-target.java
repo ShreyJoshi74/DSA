a4d30ba90062a233bfd14d67628d55de0c016833
Shrey Joshi <shreyjoshi199@gmail.com>
2026-01-31 16:40:58 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (24.49%) - LeetSync

--- bitManipulation/745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ bitManipulation/745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -3,11 +3,18 @@ class Solution {
         
         
         int ans = 'z' + 1;
-        for(int i = 0; i < letters.length; i++){
-            if(target < letters[i] && letters[i] < ans)
-                ans = letters[i] ;
+        int low = 0;
+        int high = letters.length - 1;
+        while(low <= high){
+            int mid = (low + high) >> 1;
+
+            if( target < letters[mid] && letters[mid] < ans ){
+                ans = letters[mid];
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
         }
-        
         return ans == 'z' + 1 ? letters[0] : (char) ans;
     }
 }
